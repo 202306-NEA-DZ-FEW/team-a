b35c06db001eddb3039e7e794c2ffeecf1448eff
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-10-12 22:22:01 +0100

    feat(project): fix prettier Code Check

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        futura: "futura",
-        futuraBlack: "futura-black",
-      },
-    },
-  },
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-                  
-"primary": "#27582D",
-        
-"secondary": "#6658F8",
-        
-"accent": "#FFB541",
-        
-"neutral": "#102813",
-        
-"base-100": "#F4F1EB",
-        
-"info": "#93c5fd",
-        
-"success": "#059669",
-        
-"warning": "#dc2626",
-        
-"error": "#ef4444",
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                futura: "futura",
+                futuraBlack: "futura-black",
+            },
         },
-      },],
-  },
-  plugins: [require("daisyui")],
-}
+    },
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#27582D",
+
+                    secondary: "#6658F8",
+
+                    accent: "#FFB541",
+
+                    neutral: "#102813",
+
+                    "base-100": "#F4F1EB",
+
+                    info: "#93c5fd",
+
+                    success: "#059669",
+
+                    warning: "#dc2626",
+
+                    error: "#ef4444",
+                },
+            },
+        ],
+    },
+    plugins: [require("daisyui")],
+};
