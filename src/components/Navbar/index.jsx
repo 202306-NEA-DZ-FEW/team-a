453267ccb9923aeddf8d73fd93afe73ae5e0f776
Farouk <faroukwebdesign@gmail.com>
2023-10-14 19:51:32 +0100

    style(navbar): implement headless ui, add href links to their proper pages, add additional styling

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,11 @@ import Link from "next/link";
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { TbLanguage } from "react-icons/tb";
-import { HiOutlineUserCircle } from "react-icons/hi2";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { HiOutlineUserCircle } from "react-icons/hi";
+
 function Navbar() {
   return (
     <>
@@ -18,154 +22,259 @@ function Navbar() {
           {/* Navbar Navigation Links */}
           <ul className='menu menu-horizontal px-1'>
             <li>
-              <Link href={""}>Home</Link>
+              <Link href={"/"}>Home</Link>
             </li>
             <li>
-              <Link href={""}>About Us</Link>
+              <Link href={"/about"}>About Us</Link>
             </li>
             <li>
-              <Link href={""}>Products</Link>
+              <Link href={"/products"}>Products</Link>
             </li>
             <li>
-              <Link href={""}>Blogs</Link>
+              <Link href={"/blogs"}>Blogs</Link>
             </li>
           </ul>
         </div>
         <div className='navbar-end mr-7'>
           {/* Avatar */}
-          <div className='dropdown dropdown-end'>
-            <button
-              type='button'
-              tabIndex={0}
-              className='btn btn-ghost btn-circle avatar hidden md:hidden lg:block hover:bg-white transform scale-100 hover:scale-110 transition-transform duration-200 ease-in-out'
+          <Menu as='div' className='text-left hidden lg:block'>
+            <div>
+              <Menu.Button className='w-full justify-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50 btn-circle btn-ghost'>
+                <HiOutlineUserCircle className='w-6 h-6' />
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
             >
-              <HiOutlineUserCircle className='h-6 w-6' />
-            </button>
-            <ul
-              tabIndex={0}
-              className='z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 mt-4'
+              <Menu.Items className='absolute right-0 z-10 mt-6 lg:mr-4 w-screen md:w-screen lg:w-56 origin-top-right rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                <div className='py-2 text-center mx-4'>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='#'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block rounded px-4 py-2 text-sm"
+                        )}
+                      >
+                        Dashboard
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='#'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Settings
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='#'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Listings
+                      </Link>
+                    )}
+                  </Menu.Item>
+
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        type='submit'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "w-full text-sm btn btn-sm bg-cyan-800 text-white"
+                        )}
+                      >
+                        Sign out
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+          {/* Language Filter */}
+          <Menu as='div' className='text-left hidden lg:block'>
+            <div>
+              <Menu.Button className='w-full justify-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50 btn-circle btn-ghost'>
+                <TbLanguage className='w-6 h-6' />
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
             >
-              <li>
-                <Link href={""} className='justify-center'>
-                  Profile
-                </Link>
-              </li>
-              <li className='divide-y divide-gray-200'>
-                <Link className='justify-center' href={""}>
-                  Settings
-                </Link>
-              </li>
-              <span className='divider p-0 m-0'></span>
-              <li>
-                <Link className='justify-center' href={""}>
-                  Logout
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className='dropdown dropdown-end hidden lg:block'>
-            <button
-              type='button'
-              tabIndex={0}
-              className='btn btn-ghost btn-circle avatar hidden md:hidden lg:block hover:bg-white transform scale-100 hover:scale-110 transition-transform duration-200 ease-in-out'
-            >
-              <TbLanguage className='w-6 h-6' />
-            </button>
-            <ul
-              tabIndex={0}
-              className='z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 mt-4'
-            >
-              <li>
-                <Link className='justify-center pt-2 pb-2' href='/' locale='en'>
-                  <span className='fi fi-gb rounded'></span> English
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className=' justify-center pt-2 pb-2'
-                  href='/'
-                  locale='fr'
-                >
-                  <span className='fi fi-fr rounded'></span> Français
-                </Link>
-              </li>
-              <li>
-                <Link className='justify-center pt-2 pb-2' href='/' locale='ar'>
-                  <span className='fi fi-sa rounded'></span> العربية
-                </Link>
-              </li>
-            </ul>
-          </div>
+              <Menu.Items className='absolute right-0 z-10 mt-6 lg:mr-4 w-screen md:w-screen lg:w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                <div className='py-1'>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='#'
+                        locale='en'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        <span className='fi fi-gb rounded mr-2'></span> English
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='/'
+                        locale='fr'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        <span className='fi fi-fr rounded mr-2'></span> Français
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='/'
+                        locale='ar'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        <span className='fi fi-sa rounded mr-2'></span> العربية
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+
           {/* This is Related to Mobile View Navbar Menu */}
-          <div className='dropdown dropdown-end lg:hidden'>
-            <label tabIndex={0} className='btn btn-ghost btn-circle'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-5 w-5'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 6h16M4 12h16M4 18h7'
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className='menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box'
+          <Menu as='div' className='text-left block lg:hidden'>
+            <div>
+              <Menu.Button className='w-full justify-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50 btn-circle btn-ghost'>
+                <HiOutlineMenuAlt3 className='w-6 h-6' />
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
             >
-              <li>
-                <Link href={""} className='flex justify-between'>
-                  Dashboard{" "}
-                  <span>
-                    <FaUserCircle className='h-5 w-6' />
-                  </span>
-                </Link>
-              </li>
-              <hr />
-              <li>
-                <Link href={""}>Home</Link>
-              </li>
-              <li>
-                <Link href={""}>About Us</Link>
-              </li>
-              <li>
-                <Link href={""}>Products</Link>
-              </li>
-              <li>
-                <Link href={""}>Blogs</Link>
-              </li>
-              <li>
-                <select className='select select-sm w-full'>
-                  <option disabled selected>
-                    Select a language
-                  </option>
-                  <option>
-                    {" "}
-                    <Link href='/' locale='en'>
-                      <span className='fi fi-gb'></span> English
-                    </Link>
-                  </option>
-                  <option>
-                    {" "}
-                    <Link href='/' locale='fr'>
-                      <span className='fi fi-fr'></span> Français
-                    </Link>
-                  </option>
-                  <option>
-                    {" "}
-                    <Link href='/' locale='ar'>
-                      <span className='fi fi-sa'></span> العربية
-                    </Link>
-                  </option>
-                </select>
-              </li>
-            </ul>
-          </div>
+              <Menu.Items className='absolute right-0 z-10 mt-4 lg:mr-4 w-screen md:w-screen lg:w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                <div className='py-1'>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='/'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Home
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='/about'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        About
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='/products'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Products
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href='/blogs'
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Blogs
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
         </div>
       </div>
       {/* Navbar Ends Here */}
@@ -184,6 +293,10 @@ function Navbar() {
       </Container> */}
     </>
   );
+}
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
 }
 
 export default Navbar;
