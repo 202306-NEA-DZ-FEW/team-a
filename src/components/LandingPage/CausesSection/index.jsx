aac489d406860c3f7703104653a7d8ac3fc870aa
hadiadjd <celestialgeek21@gmail.com>
2023-10-15 12:34:05 +0100

    feat(landing page): refactor code

--- src/components/LandingPage/CausesSection/index.jsx
+++ src/components/LandingPage/CausesSection/index.jsx
@@ -4,40 +4,34 @@ import { LuHeartHandshake } from "react-icons/lu";
 
 import Container from "@/components/container";
 
+import InfoCard from "../InfoCard";
+
 function CausesSection() {
   return (
     <Container className='min-h-screen flex flex-col justify-center'>
       <h1 className='text-4xl text-center font-bold font-nunito'>Causes</h1>
       <div className='flex flex-col justify-center items-start text-center p-6 md:flex-row'>
-        <div className='p-4 flex flex-col flex-1 items-center gap-4'>
-          <IoHelpBuoy className='text-6xl text-error' />
-          <h2 className='text-3xl font-bold'>Warmth</h2>
-          <p>
-            we believe that refugees & homeless deserve having access to clean
-            warm clothes in the cold winter & comfy clothes in the summer.
-          </p>
-        </div>
-        <div className='p-4 flex flex-col flex-1 items-center gap-4'>
-          <LuHeartHandshake className='text-6xl text-error' />
-          <h2 className='text-3xl font-bold'>Comfort</h2>
-          <p>
-            we seek helping people in need to find affordable furnitures &
-            appliances because everyone deserves to have a home, not just a
-            house.
-          </p>
-        </div>
-        <div className='p-4 flex flex-col flex-1 items-center gap-4'>
-          <FaHandHoldingWater className='text-6xl text-error' />
-          <h2 className='text-3xl font-bold'>Health</h2>
-          <p>
-            Let&apos;s share believes that every human being deserves a warm
-            meal whenever it&apos;s difficult.
-          </p>
-        </div>
-      </div>
+        <InfoCard
+          icon={<IoHelpBuoy className='text-6xl text-error' />}
+          title='Warmth'
+          description=' we believe that refugees & homeless deserve having access to clean
+            warm clothes in the cold winter & comfy clothes in the summer.'
+        />
 
-      {/* PiHandHeart  PiHandHeartFill PiHandHeartDuotone FaHandHoldingHeart */}
-      {/* MdOutlineHandshake */}
+        <InfoCard
+          icon={<LuHeartHandshake className='text-6xl text-error' />}
+          title='Comfort'
+          description='we seek helping people in need to find affordable furnitures &
+            appliances because everyone deserves to have a home, not just a
+            house.'
+        />
+        <InfoCard
+          icon={<FaHandHoldingWater className='text-6xl text-error' />}
+          title='Health'
+          description=" Let's share believes that every human being deserves a warm
+            meal whenever it's difficult."
+        />
+      </div>
     </Container>
   );
 }
