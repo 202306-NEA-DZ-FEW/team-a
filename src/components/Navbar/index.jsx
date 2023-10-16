eba74f5b0b285c8fe82b7211a9b0d17f31008cdc
Farouk <faroukwebdesign@gmail.com>
2023-10-16 05:02:16 +0100

    fix(unused code): delete unused code

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { useState } from "react";
 import LanguageFilterMenu from "./LanguageFilterMenu";
 import UserMenu from "./UserMenu";
 import MobileMenu from "./MobileMenu";
