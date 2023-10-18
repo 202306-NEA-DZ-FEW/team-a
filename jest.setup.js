476b98ef23706150868f9175a26ddb118afca19f
Riadh mouamnia <riad.sodmg@gmail.com>
2023-10-18 21:34:14 +0100

    fix(project): fix testing issue with i18next

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,10 @@
 import "@testing-library/jest-dom/extend-expect";
+import i18n from "i18n-testing";
+
+beforeAll(async () => {
+  await i18n.init();
+});
+
+afterAll(() => {
+  i18n.changeLanguage("en"); // Reset the language after all tests.
+});
