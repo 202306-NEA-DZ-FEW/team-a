10c50945207b41e60e81a87b15b410fa27f5c3a6
hadiadjd <celestialgeek21@gmail.com>
2023-10-26 13:14:03 +0100

    perf(about page): add translation files
    add translation files for the first section in the page.
    
    perf #54

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -14,17 +14,8 @@ function About({ t }) {
           id='text'
           className='flex flex-col justify-center items-center  p-2 md:w-1/2 gap-2'
         >
-          <h1 className='text-5xl font-futuraBlack'>Who we are</h1>
-          <p className='text-start p-4'>
-            At Unify, we&apos;re more than just a team of developers; we&apos;re
-            a family united by a shared passion for coding and a deep commitment
-            towards our best. Driven by a collective spirit, we thrive on
-            challenges and continuously push boundaries to deliver excellence.
-            We&apos;re not just building code; we&apos;re building connections
-            within the community. Our journey is marked by collaboration,
-            learning, and a genuine love for what we do. We&apos;re here to
-            innovate, inspire, and leave a lasting mark on the world of coding.
-          </p>
+          <h1 className='text-5xl font-futuraBlack'>{t("about:title")}</h1>
+          <p className='text-start p-4'>{t("about:description")}</p>
         </div>
       </div>
     </Container>
