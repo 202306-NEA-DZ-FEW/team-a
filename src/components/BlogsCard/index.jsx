d6f2d9978a5262e851547f1dc2705356dcd9f58f
Rahem <sorourrahem17@gmail.com>
2023-10-28 04:21:27 +0100

    feat(blogs page): refactor mobile view date display in BlogsCard
    fix #47-blogs-page

--- src/components/BlogsCard/index.jsx
+++ src/components/BlogsCard/index.jsx
@@ -11,9 +11,9 @@ export default function BlogsCard({ index, blog }) {
         className={`flex flex-col gap-6 justify-center items-center bg-gray-200 p-6 px-4 ${
           index % 2 === 0 ? "lg:flex-row-reverse" : "lg:flex-row"
         } `}
-        dir={i18n?.language == "ar" ? "rtl" : "ltr"}
+        dir={i18n?.language === "ar" ? "rtl" : "ltr"}
       >
-        <div>
+        <div className='relative'>
           <Image
             src={blog.img}
             alt={blog.title}
@@ -21,11 +21,15 @@ export default function BlogsCard({ index, blog }) {
             height={100}
             className='object-cover rounded-2xl'
           />
+          <span
+            className={`absolute bottom-0 left-0 bg-primary text-white p-2 text-sm lg:hidden`}
+          >
+            {blog.date}
+          </span>
         </div>
         <div className='flex flex-col gap-2 px-10 lg:w-2/4 md:w-2/4'>
-          <span className='text-sm '>{blog.date}</span>
           <h2 className='font-bold'>{blog.title}</h2>
-          <p className='text-justify'>
+          <p>
             {blog.description.slice(0, 210)}
             {blog.description.length > 210 ? " ..." : ""}
           </p>
