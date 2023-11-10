861cc04448786e5c921c939a59bd445ce4cd1ee3
hadiadjd <celestialgeek21@gmail.com>
2023-11-10 18:31:32 +0100

    feat(newsletter): add validation and state managment
    add validation and state managment to newsletter using formik and yup
    
    fix #85

--- src/components/Newsletter/NewsletterForm.jsx
+++ src/components/Newsletter/NewsletterForm.jsx
@@ -1,16 +1,38 @@
+import { useFormik } from "formik";
+import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
+import * as Yup from "yup";
 
 import Input from "../Input";
 
 function NewsletterForm() {
   const { t } = useTranslation();
-
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("invalid email").required("email is required"),
+    }),
+    onSubmit: async (values) => {
+      console.log("thank you", values);
+    },
+  });
   return (
     <div className='bg-neutral py-10 px-6  w-full'>
       <h3>Subscribe to our newsletter</h3>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <div>
-          <Input name='email' type='email' placeholder='Enter your email' />
+          <Input
+            name='email'
+            type='email'
+            placeholder='Enter your email'
+            handleChange={formik.handleChange}
+            value={formik.values.email}
+            handleBlur={formik.handleBlur}
+            error={formik.errors.email}
+            touched={formik.touched.email}
+          />
           {/* this is a spacer */}
           <div className='label' />
         </div>
@@ -18,8 +40,9 @@ function NewsletterForm() {
         <button
           className='btn btn-primary self-center rounded-3xl text-white'
           type='submit'
+          disabled={formik.isSubmitting}
         >
-          Subscribe
+          {formik.isSubmitting ? "loading" : "Subscribe"}
         </button>
       </form>
     </div>
