0044503241dcf3878f82c6a851d33cc53b742148
hadiadjd <celestialgeek21@gmail.com>
2023-11-18 21:13:16 +0100

    feat(landing page): update causes section
    update causes section cards
    
    fix #93

--- src/components/LandingPage/CausesSection/index.jsx
+++ src/components/LandingPage/CausesSection/index.jsx
@@ -9,30 +9,36 @@ import InfoCard from "../InfoCard";
 
 function CausesSection() {
   const { t } = useTranslation();
-  return (
-    <Container className='flex flex-col justify-center lg:min-h-screen my-20 lg:my-0'>
-      <h1 className='text-3xl md:text-5xl font-bold text-center'>
-        {t("landingPage:causes:title")}
-      </h1>
-      <div className='flex flex-col justify-center items-start gap-4 text-center p-6 md:flex-row'>
-        <InfoCard
-          icon={<RiGraduationCapFill className='text-6xl text-error' />}
-          title={t("landingPage:causes:education:title")}
-          description={t("landingPage:causes:education:description")}
-        />
 
-        <InfoCard
-          icon={<LuHeartHandshake className='text-6xl text-error' />}
-          title={t("landingPage:causes:solidarity:title")}
-          description={t("landingPage:causes:solidarity:description")}
-        />
-        <InfoCard
-          icon={<FaHandHoldingWater className='text-6xl text-error' />}
-          title={t("landingPage:causes:health:title")}
-          description={t("landingPage:causes:health:description")}
-        />
-      </div>
-    </Container>
+  return (
+    <div className='lg:min-h-screen my-20 p-10 bg-opacity-20 backdrop-filter backdrop-blur-md bg-slate-300 rounded-lg shadow-2xl border border-slate-50/10'>
+      <Container className='flex flex-col flex-wrap justify-center lg:min-h-screen my-20 lg:my-0'>
+        <h1 className='text-3xl md:text-5xl pb-4 font-bold text-center'>
+          {t("landingPage:causes:title")}
+        </h1>
+        <div className='flex flex-col justify-center items-start gap-4 text-center p-6 md:flex-row'>
+          <InfoCard
+            className='w-[300px] h-[300px] pt-4 px-4 flex flex-col flex-1 items-center gap-4 bg-white rounded-xl shadow-2xl border border-slate-50/10'
+            icon={<RiGraduationCapFill className='text-6xl text-error' />}
+            title={t("landingPage:causes:education:title")}
+            description={t("landingPage:causes:education:description")}
+          />
+
+          <InfoCard
+            className='w-[300px] h-[300px] pt-4 px-4 flex flex-col flex-1 items-center gap-4 bg-white rounded-xl shadow-2xl border border-slate-50/10'
+            icon={<LuHeartHandshake className='text-6xl text-error' />}
+            title={t("landingPage:causes:solidarity:title")}
+            description={t("landingPage:causes:solidarity:description")}
+          />
+          <InfoCard
+            className='w-[300px] h-[300px] pt-4 px-4 flex flex-col flex-1 items-center gap-4 bg-white rounded-xl shadow-2xl border border-slate-50/10'
+            icon={<FaHandHoldingWater className='text-6xl text-error' />}
+            title={t("landingPage:causes:health:title")}
+            description={t("landingPage:causes:health:description")}
+          />
+        </div>
+      </Container>
+    </div>
   );
 }
 
--- src/components/LandingPage/InfoCard/index.jsx
+++ src/components/LandingPage/InfoCard/index.jsx
@@ -1,6 +1,8 @@
-function InfoCard({ title, description, icon }) {
+function InfoCard({ title, description, icon, className }) {
   return (
-    <div className='lg:p-4 flex flex-col flex-1 items-center gap-4'>
+    <div
+      className={`${className} w-[300px] h-[300px] pt-4 px-4 flex flex-col flex-1 items-center gap-4 bg-white x`}
+    >
       {icon}
       <h2 className='text-2xl md:text-3xl font-bold'>{title}</h2>
       <p className='text-md font-light'>{description}</p>
